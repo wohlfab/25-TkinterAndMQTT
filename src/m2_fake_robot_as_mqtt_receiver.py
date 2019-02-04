08a53ddd2f84566f276ec868660b70f2b1196f68
wohlfab <wohlfab@rose-hulman.edu>
2019-02-04 14:04:36 -0500

    Prep 25

--- src/m2_fake_robot_as_mqtt_receiver.py
+++ src/m2_fake_robot_as_mqtt_receiver.py
@@ -1,7 +1,32 @@
-# TODO: Copy the code in
+# DONE: Copy the code in
 #     m1e_mqtt_receiver.py
 # as your starting point, pasting its code here.
 
+import mqtt_remote_method_calls as com
+import time
+
+
+class DelegateThatReceives(object):
+
+    def say_it(self, message):
+        print("Message received!", message)
+
+
+def main():
+    name1 = input("Enter one name (subscriber): ")
+    name2 = input("Enter another name (publisher): ")
+
+    my_delegate = DelegateThatReceives()
+    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client.connect(name1, name2)
+    time.sleep(1)  # Time to allow the MQTT setup.
+    print()
+
+    while True:
+        time.sleep(0.01)  # Time to allow message processing
+
+
+main()
 # Then modify the code so that it receives messages from your
 #    m2_tkinter_as_mqtt_sender.py
 # module and PRINTS them.
--- src/m2_tkinter_as_mqtt_sender.py
+++ src/m2_tkinter_as_mqtt_sender.py
@@ -2,7 +2,7 @@
 Using a fake robot as the receiver of messages.
 """
 
-# TODO: 1. In mqtt_remote_method_calls, set LEGO_NUMBER at line 131
+# DONE: 1. In mqtt_remote_method_calls, set LEGO_NUMBER at line 131
 # to YOUR robot's number.
 
 # TODO: 2. Copy your Tkinter/ttk ROBOT gui code from the previous session (m6).
@@ -13,3 +13,91 @@ Using a fake robot as the receiver of messages.
 #   where X and Y are from the entry box.
 #
 # Implement and test.
+"""
+This project lets you try out Tkinter/Ttk and practice it!
+
+Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
+         Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
+         and Brandon Wohlfarth.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+import tkinter
+from tkinter import ttk
+
+
+def main():
+    """ Constructs a GUI that will be used MUCH later to control EV3. """
+    # -------------------------------------------------------------------------
+    # DONE: 2. Follow along with the video to make a remote control GUI
+    # For every grid() method call you will add a row and a column argument
+    # -------------------------------------------------------------------------
+
+    root = tkinter.Tk()
+    root.title("MQTT Remote")
+
+    main_frame = ttk.Frame(root, padding=20)
+    main_frame.grid()  # only grid call that does NOT need a row and column
+
+    left_speed_label = ttk.Label(main_frame, text="Left")
+    left_speed_label.grid(row = 1, column = 0)
+    left_speed_entry = ttk.Entry(main_frame, width=8)
+    left_speed_entry.insert(0, "600")
+    left_speed_entry.grid(row = 0, column = 0)
+
+    right_speed_label = ttk.Label(main_frame, text="Right")
+    right_speed_label.grid(row = 1, column = 7)
+    right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    right_speed_entry.insert(0, "600")
+    right_speed_entry.grid(row = 0, column = 7)
+
+    forward_button = ttk.Button(main_frame, text="Forward")
+    forward_button.grid(row = 3, column = 3)
+    forward_button['command'] = lambda: print("Forward button")
+    root.bind('<Up>', lambda event: print("Forward key"))
+
+    left_button = ttk.Button(main_frame, text="Left")
+    left_button.grid(row = 4, column = 2)
+    left_button['command'] = lambda: print("Left button")
+    root.bind('<Left>', lambda event: print("Left key"))
+
+    stop_button = ttk.Button(main_frame, text="Stop")
+    stop_button.grid(row = 7, column = 0)
+    stop_button['command'] = lambda: print("Stop button")
+    root.bind('<space>', lambda event: print("Stop key"))
+
+    right_button = ttk.Button(main_frame, text="Right")
+    right_button.grid(row = 4, column = 4)
+    right_button['command'] = lambda: print("Right button")
+    root.bind('<Right>', lambda event: print("Right key"))
+
+    back_button = ttk.Button(main_frame, text="Back")
+    back_button.grid(row = 5, column = 3)
+    back_button['command'] = lambda: print("Back button")
+    root.bind('<Down>', lambda event: print("Back key"))
+
+    up_button = ttk.Button(main_frame, text="Up")
+    up_button.grid(row = 7, column = 7)
+    up_button['command'] = lambda: print("Up button")
+    root.bind('<u>', lambda event: print("Up key"))
+
+    down_button = ttk.Button(main_frame, text="Down")
+    down_button.grid(row = 8, column = 7)
+    down_button['command'] = lambda: print("Down button")
+    root.bind('<j>', lambda event: print("Down key"))
+
+    # Buttons for quit and exit
+    q_button = ttk.Button(main_frame, text="Quit")
+    q_button.grid()
+    q_button['command'] = lambda: print("Quit button")
+
+    e_button = ttk.Button(main_frame, text="Exit")
+    e_button.grid()
+    e_button['command'] = lambda: exit()
+
+    root.mainloop()
+
+
+# -----------------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# -----------------------------------------------------------------------------
+main()
